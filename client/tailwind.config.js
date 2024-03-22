589b9b42993c6644787c13438ffbcc0a342f38ef
Sam Sweeney <samsweeneyy@gmail.com>
2024-03-22 16:54:35 +1100

    Redo to js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        primary: "#FFB413",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
