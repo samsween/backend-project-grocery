3a3bccc488e153a549672bb0b9832f5df7f4fb53
Sam Sweeney <samsweeneyy@gmail.com>
2024-03-30 16:37:21 +1100

    COmmit

--- client/src/pages/loading.jsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from "../components/Loader";
-
-export const Loading = () => {
-  return (
-    <div className="h-full w-full  flex items-center justify-center">
-      <Loader />
-    </div>
-  );
-};
