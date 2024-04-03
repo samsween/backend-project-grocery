d9b73f7e5fca4be852058502b505701668c91b96
Sam Sweeney <samsweeneyy@gmail.com>
2024-04-03 13:14:29 +1100

    .

--- client/src/routes/_admin.orders.jsx
+++ client/src/routes/_admin.orders.jsx
@@ -4,6 +4,7 @@ export const Route = createFileRoute("/_admin/orders")({
   component: () => (
     <div className="px-10 pt-10 flex gap-2 w-full">
       <Orders />
+      
       {/* <AddProductForm /> */}
     </div>
   ),
